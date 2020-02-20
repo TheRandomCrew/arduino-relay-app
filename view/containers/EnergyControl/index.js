5b5baf8954837dcf9bcde900be2606bcabf4044b
Israel-Laguan <israellaguan@gmail.com>
2020-02-20 16:34:05 -0500

    [fix] arduino number changed
    What?:
    
    Breaking Changes?

--- view/containers/EnergyControl/index.js
+++ view/containers/EnergyControl/index.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react'
+const arduinoNumber = 50370093968;
 
 const EnergyControl = () => {
   const [status, setStatus] = useState('off')
 
   const pressOn = () => {
-    fetch("https://arduino-relay-backend.herokuapp.com/50361855000/e")
+    fetch(`https://arduino-relay-backend.herokuapp.com/${arduinoNumber}/e`)
       .then(res => res.json())
       .then(res=>{
         console.log(res)
@@ -14,7 +15,7 @@ const EnergyControl = () => {
   }
 
   const pressOff = () => {
-    fetch("https://arduino-relay-backend.herokuapp.com/50361855000/e")
+    fetch(`https://arduino-relay-backend.herokuapp.com/${arduinoNumber}/e`)
       .then(res => res.json())
       .then(res=>{
         console.log(res)
